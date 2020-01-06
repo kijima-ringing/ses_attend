bb3131736c410a1977de6a53e3f63e7b4de45257
n.ishii <n.ishii@adseed.co.jp>
2020-01-06 21:44:49 +0900

    ユーザー初期データ作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'last_name' => '山田',
+                'first_name' => '太郎',
+                'last_name_kana' => 'ヤマダ',
+                'first_name_kana' => 'タロウ',
+                'email' => 'test001@example.com',
+                'password' => Hash::make('test123+'),
+                'admin_flag' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        ]);
     }
 }
