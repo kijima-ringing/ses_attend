53ce78fffccc80f9193ac47e00377c681a152d9a
n.ishii <n.ishii@adseed.co.jp>
2020-01-06 21:31:14 +0900

    ユーザーテーブルのマイグレーションにカラム追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,6 +27,7 @@ class CreateUsersTable extends Migration
             $table->softDeletes();
             $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
             $table->timestamps();
         });
     }
