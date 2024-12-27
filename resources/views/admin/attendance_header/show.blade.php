6f7f37978e1c6d1c9e5ac83d2bb161423ff8b6f5
kijima-ringing <kijima.masaru@ringing.co.jp>
2024-12-27 15:45:12 +0900

    answer7：モーダル内微修正

--- resources/views/admin/attendance_header/show.blade.php
+++ resources/views/admin/attendance_header/show.blade.php
@@ -239,7 +239,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">有給休暇申請詳細</h5>
+                <h5>有給申請詳細</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
                     <span aria-hidden="true">&times;</span>
                 </button>
